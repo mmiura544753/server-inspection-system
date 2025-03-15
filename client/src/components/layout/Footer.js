61194e1412a59951bfe3172823bc884f80a805d0
miura <mmiura544753@gmail.com>
2025-03-15 20:29:12 +0900

    コピーライト修正

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,13 +1,13 @@
 // src/components/layout/Footer.js
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
-  
+
   return (
     <footer className="app-footer">
       <div className="container">
-        <p className="m-0">© {currentYear} サーバー点検管理システム</p>
+        <p className="m-0">© {currentYear} Zukosha Co., Ltd.</p>
       </div>
     </footer>
   );

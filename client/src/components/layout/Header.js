be274eded51bfec717d67c451ccb042e72781b5f
miura <mmiura544753@gmail.com>
2025-03-15 19:43:12 +0900

    ヘッダーを修正

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -1,23 +1,29 @@
 // src/components/layout/Header.js
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className="app-header">
       <nav className="navbar navbar-expand-lg navbar-dark">
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">サーバー点検システム</Link>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+          <Link className="navbar-brand" to="/">
+            サーバー点検システム
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+          >
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ms-auto">
               <li className="nav-item">
-                <Link className="nav-link" to="/">ホーム</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/customers">顧客管理</Link>
+                <Link className="nav-link" to="/">
+                  ホーム
+                </Link>
               </li>
             </ul>
           </div>
@@ -28,9 +34,3 @@ const Header = () => {
 };
 
 export default Header;
-
-
-
-
-
-

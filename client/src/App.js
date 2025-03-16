253a6bec36610a0362693ec350b88a77ba964ca9
miura <mmiura544753@gmail.com>
2025-03-16 21:16:34 +0900

    修正

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
 // src/App.js
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./App.css";
